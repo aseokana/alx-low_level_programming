a9532256a4d96498b55929ed4a929c3390522421
aseokana <aseokana@gmail.com>
2023-02-17 11:50:40 -0800

    alphabets without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if(l != 'e' && l != 'q'){
+			putchar(l);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
