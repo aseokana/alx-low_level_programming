b0cfd420705a435fc0cb46a8025a4c9dddde4bee
aseokana <aseokana@gmail.com>
2023-02-17 15:03:02 -0800

    all possible combinations of two two-digit numbers 0-99.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - all possible combinations of two two-digit numbers 0-99.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int num, num1;
+
+	for (num = 0; num <= 98; num++)
+	{
+		for (num1 = num + 1; num1 <= 99; num1++)
+		{
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+
+			if ( num != 98 && (num1 % 10) + '0' != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
