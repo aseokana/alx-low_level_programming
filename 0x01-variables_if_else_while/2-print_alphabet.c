c306fbac127d6c0b3c2c5bd3c9284d27a3b08b60
aseokana <aseokana@gmail.com>
2023-02-17 11:09:05 -0800

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
