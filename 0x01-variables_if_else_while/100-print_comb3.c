a3bf2d372e6c2e4cba9b79e83054d320af486679
aseokana <aseokana@gmail.com>
2023-02-17 13:35:04 -0800

    all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,19 +9,21 @@ int main(void)
 {
         int num, num1;
 
-        for (num = 0; num < 9; num++)
+        for (num = 0; num <= 9; num++)
         {
-                for (num1 = num + 1; num1 < 10; num1++)
+                for (num1 = 0; num1 <= 9; num1++)
                 {
-                        putchar((num % 10) + '0');
-                        putchar((num1 % 10) + '0');
+			if (num < num1)
+			{
+                        	putchar((num % 10) + '0');
+                        	putchar((num1 % 10) + '0');
 
-                        if (num != 8 && num1 != 9)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                        
+				if (num != '8' || (num == '8' && num1 != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
                 }
         }
 
