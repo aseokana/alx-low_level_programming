4e061f3b8265cfa043a16aa88ce95f82188af862
aseokana <aseokana@gmail.com>
2023-02-21 14:02:37 -0800

    a function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description: a function that adds two integers
+ * @a: The first integer input.
+ * @b: The second integer input.
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
