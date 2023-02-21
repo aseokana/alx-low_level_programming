8f6c3285b2e955d6602dad9971b9bec315e3e729
aseokana <aseokana@gmail.com>
2023-02-21 10:20:39 -0800

    lowercase alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: prints lowercase alphabets
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		char abc;
+		for (abc = 'a'; abc <= 'z'; abc++)
+		{
+			_putchar(abc);
+		}
+		_putchar('\n');
+	}
+}
